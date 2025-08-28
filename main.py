1c85cc5c4e1b9be80563ed76be47a1ee67c9208a
Gabriel Felipe <gabrielfelipe11102002@gmail.com>
2025-08-28 11:37:34 -0300

    Desenvolvimento do main.py

--- main.py
+++ main.py
@@ -31,3 +31,35 @@ while (sair):
 
     menu_inicial.interface()
     escolha = menu_inicial.input()
+    
+    match escolha:
+        case 0:
+            print("Saindo do aplicativo")
+            sair = False;
+        case 1:
+            print(cadastrar_cliente())
+        case 2:
+            get_all_clientes()
+        case 3:
+            get_all_clientes()
+            print(alterar_endereco_cliente())
+        case 4:
+            print(cadastrar_funcionario())
+        case 5:
+            print(get_all_funcionarios())
+        case 6:
+            print(alterar_endereco_funcionario())
+        case 7:
+            print(demitir_funcionario())
+        case 8:
+            print(promover_funcionario())
+        case 9:
+            print(adicionar_veiculo())
+        case 10:
+            print(get_all_veiculos())
+        case 11:
+            print(tirar_veiculo_frota())
+        case 12:
+            print(fazer_reserva())
+        case 13:
+            print(get_all_reservas())
